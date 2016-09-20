aefc7248281a35b22271abaa2231a527ba76a8cd
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-20 12:09:34 +0200

    Add sentry recipe

--- recipes/sentry.php
+++ recipes/sentry.php
@@ -4,4 +4,11 @@
  * Notify Sentry about deployment.
  */
 task('sentry:notify_deployment', function () {
+    $org = get('sentry_org');
+    $project = get('sentry_project');
+
+    $data = ['version' => '{{release_name}}', 'ref' => '{{release_name}}', 'dateReleased' => date('Y-m-d H:i:s')];
+    $json = json_encode($data);
+
+    runLocally("curl -s -d \"payload=$json\" https://app.getsentry.com/api/0/projects/$org/$project/releases/");
 })->desc('Notify Sentry about deployment');
