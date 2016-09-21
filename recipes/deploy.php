6a011d0dc8a11b31a3ebbfaa09f2a73f081b9122
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-21 13:20:58 +0200

    Fix permissions user and group

--- recipes/deploy.php
+++ recipes/deploy.php
@@ -15,21 +15,30 @@ set('group', 'www-data');
  * Set right user and group on root files.
  */
 task('deploy:groupify_root', function () {
-    run("test -d {{deploy_path}}/ && sudo chown -R {{user}}:{{group}} {{deploy_path}}/");
+    $user  = get('user');
+    $group = get('group');
+
+    run("test -d {{deploy_path}}/ && sudo chown -R $user:$group {{deploy_path}}/");
 })->desc('Set right permissions on root directory');
 
 /**
  * Set right user and group on releases files.
  */
 task('deploy:groupify_releases', function () {
-    run("test -d {{deploy_path}}/releases/ && sudo chown -R {{user}}:{{group}} {{deploy_path}}/releases/");
+    $user  = get('user');
+    $group = get('group');
+
+    run("test -d {{deploy_path}}/releases/ && sudo chown -R $user:$group {{deploy_path}}/releases/");
 })->desc('Set right permissions on releases directory');
 
 /**
  * Set right user and group on shared files.
  */
 task('deploy:groupify_shared', function () {
-    run("test -d {{deploy_path}}/shared/ && sudo chown -R {{user}}:{{group}} {{deploy_path}}/shared/");
+    $user  = get('user');
+    $group = get('group');
+
+    run("test -d {{deploy_path}}/shared/ && sudo chown -R $user:$group {{deploy_path}}/shared/");
 })->desc('Set right permissions on shared files');
 
 /**
@@ -85,6 +94,7 @@ task('deploy:vendors', function () {
 /**
  * Add before and after hooks.
  */
+before('deploy:prepare', 'deploy:groupify_root');
 before('deploy:update_code', 'deploy:groupify_root');
 before('rollback', 'deploy:groupify_releases');
 after('deploy:update_code', 'deploy:groupify_releases');
