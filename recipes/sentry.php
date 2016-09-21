4b44e821e4438b64669358e68f494984b48a58a6
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-21 10:17:23 +0200

    Fix sentry task

--- recipes/sentry.php
+++ recipes/sentry.php
@@ -6,9 +6,11 @@
 task('sentry:notify_deployment', function () {
     $org = get('sentry_org');
     $project = get('sentry_project');
+    $key = get('sentry_api_key');
 
     $data = ['version' => '{{release_name}}', 'ref' => '{{release_name}}', 'dateReleased' => date('Y-m-d H:i:s')];
     $json = json_encode($data);
+    $json = addcslashes($json, '"\\/');
 
-    runLocally("curl -s -d \"payload=$json\" https://app.getsentry.com/api/0/projects/$org/$project/releases/");
+    runLocally("curl --user $key:\"\" -s -d \"$json\" -H \"Content-Type: application/json\" https://app.getsentry.com/api/0/projects/$org/$project/releases/");
 })->desc('Notify Sentry about deployment');
