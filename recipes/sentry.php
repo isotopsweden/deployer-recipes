ec1696f8da7e393ad09ca1c7c66940590d230063
Rasmus Bengtsson <rasmus@isotop.se>
2016-11-07 14:00:32 +0100

    Fix when missing sentry info

--- recipes/sentry.php
+++ recipes/sentry.php
@@ -4,9 +4,14 @@
  * Notify Sentry about deployment.
  */
 task('sentry:notify_deployment', function () {
-    $org = get('sentry_org');
-    $project = get('sentry_project');
-    $key = get('sentry_api_key');
+    try {
+        $org = get('sentry_org');
+        $project = get('sentry_project');
+        $key = get('sentry_api_key');
+    } catch (\RuntimeException $exception) {
+        println('<info>Missing Sentry settings, will not notify about release</info>');
+        return;
+    }
 
     $data = ['version' => '{{release_name}}', 'ref' => '{{release_name}}', 'dateReleased' => date('Y-m-d H:i:s')];
     $json = json_encode($data);
