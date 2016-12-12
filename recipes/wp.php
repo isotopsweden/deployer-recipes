3252646188eee219d3493871ffb8d83dceeeb005
Rasmus Bengtsson <rasmus@isotop.se>
2016-12-12 16:58:10 +0100

    Add wpdb upgrade task

--- recipes/wp.php
+++ recipes/wp.php
@@ -6,3 +6,10 @@
 task('wp:flushall', function () {
     run('[ -f /usr/local/bin/wp ] && cd {{deploy_path}}/current && for i in $(/usr/local/bin/wp site list --fields=url --format=csv); do if [ \"$i\" != \"url\" ]; then /bin/echo -n \"$i: \" && /usr/local/bin/wp rewrite flush --url=$i; fi; done || echo "wp-cli is not installed"');
 })->desc('Flushes all sites permalinks using wp-cli');
+
+/**
+ * Flushes all sites permalinks.
+ */
+task('wp:dbupgrade', function () {
+    run('[ -f /usr/local/bin/wp ] && cd {{deploy_path}}/current && for i in $(/usr/local/bin/wp site list --fields=url --format=csv); do if [ \"$i\" != \"url\" ]; then /bin/echo -n \"$i: \" && /usr/local/bin/wp core update-db --url=$i; fi; done || echo "wp-cli is not installed"');
+})->desc('Upgrades all databases using wp-cli');
